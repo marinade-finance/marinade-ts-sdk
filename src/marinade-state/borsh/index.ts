054f04494b6143be47c3df390f351fa127050264
janlegner <32453746+janlegner@users.noreply.github.com>
2021-11-29 18:53:05 +0100

    Exporting more types from the library (#2)

---
+++ src/marinade-state/borsh/index.ts
@@ -0,0 +1,6 @@
+export { MARINADE_BORSH_SCHEMA } from './marinade-borsh'
+export { deserializeF64, deserializePublicKey } from './common'
+export { StakeInfo } from './stake-info'
+export { StakeRecord } from './stake-record'
+export { Authorized, Delegation, Lockup, Meta, Stake, StakeState } from './stake-state'
+export { ValidatorRecord } from './validator-record'
