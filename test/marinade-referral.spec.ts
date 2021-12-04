866c807841cecff9135d9bd3c1c97ccc738e9baf
Jan Legner <jan.legner@gmail.com>
2021-12-04 16:07:26 +0100

    Better tests

---
+++ test/marinade-referral.spec.ts
@@ -0,0 +1,44 @@
+import { Marinade, MarinadeConfig, MarinadeUtils } from '../src'
+import * as TestWorld from './test-world'
+
+const MINIMUM_LAMPORTS_BEFORE_TEST = MarinadeUtils.solToLamports(2)
+
+describe('Marinade Referral', () => {
+  console.log('SDK User', TestWorld.SDK_USER.publicKey.toBase58())
+
+  beforeAll(async () => {
+    await TestWorld.provideMinimumLamportsBalance(TestWorld.SDK_USER.publicKey, MINIMUM_LAMPORTS_BEFORE_TEST)
+  })
+
+  describe('deposit', () => {
+    it('deposits SOL', async () => {
+      console.log('ReferralCode:', TestWorld.REFERRAL_CODE.toBase58())
+
+      const config = new MarinadeConfig({
+        anchorProviderUrl: TestWorld.PROVIDER_URL,
+        referralCode: TestWorld.REFERRAL_CODE,
+        wallet: TestWorld.SDK_USER,
+      })
+      const marinade = new Marinade(config)
+
+      const { transactionSignature } = await marinade.deposit(MarinadeUtils.solToLamports(1))
+      console.log('Deposit tx:', transactionSignature)
+    })
+  })
+
+  describe('liquidUnstake', () => {
+    it('unstakes SOL', async () => {
+      console.log('ReferralCode:', TestWorld.REFERRAL_CODE.toBase58())
+
+      const config = new MarinadeConfig({
+        anchorProviderUrl: TestWorld.PROVIDER_URL,
+        referralCode: TestWorld.REFERRAL_CODE,
+        wallet: TestWorld.SDK_USER,
+      })
+      const marinade = new Marinade(config)
+
+      const { transactionSignature } = await marinade.liquidUnstake(MarinadeUtils.solToLamports(0.8))
+      console.log('Liquid unstake tx:', transactionSignature)
+    })
+  })
+})
