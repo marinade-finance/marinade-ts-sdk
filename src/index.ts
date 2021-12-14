98f16b411191ac391b9f9ebe387ad6e194002f04
Jan Legner <jan.legner@gmail.com>
2021-12-14 08:37:33 +0100

    Added missing export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { Marinade } from './marinade'
 export { MarinadeConfig } from './config/marinade-config'
 export * as MarinadeBorsh from './marinade-state/borsh/index'
+export { MarinadeReferralPartnerState } from './marinade-referral-state/marinade-referral-partner-state'
 export { MarinadeState } from './marinade-state/marinade-state'
 export { MarinadeMint } from './marinade-mint/marinade-mint'
 export * as MarinadeUtils from './util/index'
