093fca49374a99495b433515a7f68032631d06ee
Jan Legner <jan.legner@gmail.com>
2022-01-21 19:57:32 +0100

    Added test for a single ATA creation

--- test/marinade-finance-instructions.spec.ts
+++ test/marinade-finance-instructions.spec.ts
@@ -1,5 +1,6 @@
-import { Marinade, MarinadeConfig, MarinadeUtils, Wallet, web3 } from '../src'
+import { Marinade, MarinadeConfig, MarinadeUtils, Provider, Wallet, web3 } from '../src'
 import * as TestWorld from './test-world'
+import assert from 'assert'
 
 const MINIMUM_LAMPORTS_BEFORE_TEST = MarinadeUtils.solToLamports(2.5)
 
@@ -13,20 +14,46 @@ describe('Marinade Finance', () => {
       const config = new MarinadeConfig({
         connection: TestWorld.CONNECTION,
         publicKey: TestWorld.SDK_USER.publicKey,
-        referralCode: TestWorld.REFERRAL_CODE,
       })
       const marinade = new Marinade(config)
 
       const { transaction } = await marinade.deposit(MarinadeUtils.solToLamports(1))
       const transactionSignature = await TestWorld.PROVIDER.send(transaction)
-      console.log('Deposit tx:', transactionSignature)
+      console.log('Deposit tx:', transactionSignature, transaction.instructions.length)
+    })
+
+    it('deposits SOL, only creates ATA when necessary', async() => {
+      const newAccount = new web3.Keypair()
+      await TestWorld.provideMinimumLamportsBalance(newAccount.publicKey, MarinadeUtils.solToLamports(2.5))
+
+      const provider = new Provider(
+        TestWorld.CONNECTION,
+        new Wallet(newAccount),
+        { commitment: 'finalized' },
+      )
+
+      const anotherAccount = web3.Keypair.generate()
+      const config = new MarinadeConfig({
+        connection: TestWorld.CONNECTION,
+        publicKey: newAccount.publicKey,
+      })
+      const marinade = new Marinade(config)
+
+      const { transaction: tx1 } = await marinade.deposit(MarinadeUtils.solToLamports(1), { mintToOwnerAddress: anotherAccount.publicKey })
+      assert.strictEqual(tx1.instructions.length, 2)
+      const transactionSignature1 = await provider.send(tx1)
+      console.log('Deposit tx1:', transactionSignature1)
+
+      const { transaction: tx2 } = await marinade.deposit(MarinadeUtils.solToLamports(1),{ mintToOwnerAddress: anotherAccount.publicKey })
+      assert.strictEqual(tx2.instructions.length, 1)
+      const transactionSignature2 = await provider.send(tx2)
+      console.log('Deposit tx2:', transactionSignature2)
     })
 
     it('deposits SOL and get mSOL to another account', async() => {
       const config = new MarinadeConfig({
         connection: TestWorld.CONNECTION,
         publicKey: TestWorld.SDK_USER.publicKey,
-        referralCode: TestWorld.REFERRAL_CODE,
       })
       const marinade = new Marinade(config)
 
@@ -42,7 +69,6 @@ describe('Marinade Finance', () => {
       const config = new MarinadeConfig({
         connection: TestWorld.CONNECTION,
         publicKey: TestWorld.SDK_USER.publicKey,
-        referralCode: TestWorld.REFERRAL_CODE,
       })
       const marinade = new Marinade(config)
 
@@ -59,7 +85,6 @@ describe('Marinade Finance', () => {
       const config = new MarinadeConfig({
         connection: TestWorld.CONNECTION,
         publicKey: Wallet.local().payer.publicKey,
-        referralCode: TestWorld.REFERRAL_CODE,
       })
       const marinade = new Marinade(config)
 
