bd5d0531ef3f3c3a055fd03f49cce20c6a77fa36
phola <info@phola.com>
2022-06-14 17:22:21 +0100

    feat: add claim in last 4 hours

--- src/util/ticket-date-info.spec.ts
+++ src/util/ticket-date-info.spec.ts
@@ -1,38 +1,57 @@
-import { getTicketDateInfo } from './ticket-date-info'
+import { getTicketDateInfo } from "./ticket-date-info"
 import { ProcessedEpochInfo } from "./anchor.types"
 
 const EXTRA_WAIT_MILLISECONDS = 1000 * 60 * 60 * 4 + 1000 * 60 * 45
 
 const currentEpoch: ProcessedEpochInfo = {
-  slotsInEpoch: 432000, 
-  absoluteSlot: 137505844, 
-  epoch: 318, 
+  slotsInEpoch: 432000,
+  absoluteSlot: 137505844,
+  epoch: 318,
   msElapsed: 95225546,
-  msUntilEpochEnd: 221596455, 
+  msUntilEpochEnd: 221596455,
   avgSlotDuration: 733,
   slotIndex: 129844,
   epochProgress: 30,
 }
 
-describe('ticket-date-info', () => {
+describe("ticket-date-info", () => {
+  describe("getTicketDateInfo", () => {
+    it("ticket date info created same epoch is not due and correct", () => {
+      const actualResult = getTicketDateInfo(currentEpoch, 318, 1655201767918)
 
-  describe('getTicketDateInfo', () => {
-    it('ticket date info created same epoch is correct', () => {
-      const actualResult = getTicketDateInfo(currentEpoch, 318, 1655201767918) 
-     
-      expect(actualResult).toEqual({ticketDueDate: new Date(1655201767918 + 221596455 + EXTRA_WAIT_MILLISECONDS), ticketDue: false})
+      expect(actualResult).toEqual({
+        ticketDueDate: new Date("2022-06-17T04:34:24.373Z"),
+        ticketDue: false,
+      })
     })
-    it('ticket date info created previous epoch is correct', () => {
-      const actualResult = getTicketDateInfo(currentEpoch, 317, 1655201767918) 
-     
-      expect(actualResult).toEqual({ticketDueDate: new Date(1655201767918 - 95225546 + EXTRA_WAIT_MILLISECONDS), ticketDue: true})
+    it("ticket date info created previous epoch is due and correct", () => {
+      const actualResult = getTicketDateInfo(currentEpoch, 317, 1655201767918)
+
+      expect(actualResult).toEqual({
+        ticketDueDate: new Date("2022-06-13T12:34:02.372Z"),
+        ticketDue: true,
+      })
     })
-    it('ticket date info created 3 epochs ago is correct', () => {
-      const actualResult = getTicketDateInfo(currentEpoch, 315, 1655201767918) 
-     
-      expect(actualResult).toEqual({ticketDueDate: new Date('2022-06-06T04:38:50.372Z'), ticketDue: true})
+    it("ticket date info created within last 4 hours of previous epoch is not due and correct", () => {
+      currentEpoch.msUntilEpochEnd = EXTRA_WAIT_MILLISECONDS - 1000 * 60
+      const actualResult = getTicketDateInfo(
+        currentEpoch,
+        317,
+        1655201767918 + 221596455 - currentEpoch.msUntilEpochEnd
+      )
+
+      expect(actualResult).toEqual({
+        ticketDueDate: new Date("2022-06-20T20:32:00.373Z"),
+        ticketDue: false,
+      })
     })
- 
+    it("ticket date info created 3 epochs ago is due and correct", () => {
+      const actualResult = getTicketDateInfo(currentEpoch, 315, 1655201767918)
+
+      expect(actualResult).toEqual({
+        ticketDueDate: new Date("2022-06-06T04:38:50.372Z"),
+        ticketDue: true,
+      })
+    })
   })
-
 })
--- src/util/ticket-date-info.ts
+++ src/util/ticket-date-info.ts
@@ -12,20 +12,38 @@ import { TicketDateInfo } from "./ticket-date-info.types"
 // https://docs.marinade.finance/marinade-protocol/system-overview#delayed-unstaked
 const EXTRA_WAIT_MILLISECONDS = 1000 * 60 * 60 * 4 + 1000 * 60 * 45
 
-export function getTicketDateInfo(currentEpoch: ProcessedEpochInfo, createdEpochNumber: number, currentTime:number = Date.now()): TicketDateInfo {
-  
+export function getTicketDateInfo(
+  currentEpoch: ProcessedEpochInfo,
+  createdEpochNumber: number,
+  currentTime: number = Date.now()
+): TicketDateInfo {
   const currentEpochStart = currentTime - currentEpoch.msElapsed
   const currentEpochEnd = currentTime + currentEpoch.msUntilEpochEnd
-  const estimatedEpochDuration = currentEpoch.avgSlotDuration * currentEpoch.slotsInEpoch
+  const estimatedEpochDuration =
+    currentEpoch.avgSlotDuration * currentEpoch.slotsInEpoch
 
   if (createdEpochNumber < currentEpoch.epoch) {
-    const dueDate = 
-      new Date((currentEpochStart + EXTRA_WAIT_MILLISECONDS) - estimatedEpochDuration * (currentEpoch.epoch - createdEpochNumber - 1))
-    return {  ticketDue: true, 
-      ticketDueDate: dueDate,
+    if (
+      createdEpochNumber === currentEpoch.epoch - 1 &&
+      currentEpoch.msUntilEpochEnd < EXTRA_WAIT_MILLISECONDS
+    ) {
+      return {
+        ticketDue: false,
+        ticketDueDate: new Date(
+          currentEpochEnd + EXTRA_WAIT_MILLISECONDS + estimatedEpochDuration
+        ),
+      }
     }
+    const dueDate = new Date(
+      currentEpochStart +
+        EXTRA_WAIT_MILLISECONDS -
+        estimatedEpochDuration * (currentEpoch.epoch - createdEpochNumber - 1)
+    )
+    return { ticketDue: true, ticketDueDate: dueDate }
   }
 
-  return { ticketDue: false, ticketDueDate: new Date(currentEpochEnd + EXTRA_WAIT_MILLISECONDS) }
-
+  return {
+    ticketDue: false,
+    ticketDueDate: new Date(currentEpochEnd + EXTRA_WAIT_MILLISECONDS),
+  }
 }
