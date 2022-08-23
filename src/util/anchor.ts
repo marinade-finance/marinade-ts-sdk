5dd1ff640fdab607ff552a4d0e502c236130a744
MjCage <lifepathapps@gmail.com>
2022-08-23 15:27:11 +0200

    chore: bump spl-token

--- src/util/anchor.ts
+++ src/util/anchor.ts
@@ -1,6 +1,6 @@
-import { BN, Provider, web3 } from '@project-serum/anchor'
+import { BN, web3 } from '@project-serum/anchor'
 import * as anchor from '@project-serum/anchor'
-import { AccountInfo, ASSOCIATED_TOKEN_PROGRAM_ID, Token, TOKEN_PROGRAM_ID } from '@solana/spl-token'
+import { ASSOCIATED_TOKEN_PROGRAM_ID, createAssociatedTokenAccountInstruction, getAccount, TokenAccountNotFoundError, TOKEN_PROGRAM_ID } from '@solana/spl-token'
 import { ParsedStakeAccountInfo, ProcessedEpochInfo } from './anchor.types'
 
 export const SYSTEM_PROGRAM_ID = new web3.PublicKey('11111111111111111111111111111111')
@@ -15,16 +15,8 @@ export function BNOrNull(value: ConstructorParameters<typeof BN>[0] | null): BN 
   return value === null ? null : new BN(value)
 }
 
-export function getMintClient(anchorProvider: Provider, mintAddress: web3.PublicKey): Token {
-  return new Token(anchorProvider.connection, mintAddress, TOKEN_PROGRAM_ID, web3.Keypair.generate())
-}
-
 export async function getAssociatedTokenAccountAddress(mint: web3.PublicKey, owner: web3.PublicKey): Promise<web3.PublicKey> {
   return anchor.utils.token.associatedAddress({ mint, owner })
-}
-
-export async function getTokenAccountInfo(mintClient: Token, publicKey: web3.PublicKey): Promise<AccountInfo> {
-  return mintClient.getAccountInfo(publicKey)
 }
 
 export async function getOrCreateAssociatedTokenAccount(anchorProvider: anchor.Provider, mintAddress: web3.PublicKey, ownerAddress: web3.PublicKey, payerAddress?: web3.PublicKey): Promise<{
@@ -37,22 +29,20 @@ export async function getOrCreateAssociatedTokenAccount(anchorProvider: anchor.P
   const associatedTokenAccountAddress = existingTokenAccount?.pubkey ?? await getAssociatedTokenAccountAddress(mintAddress, ownerAddress)
   let createAssociateTokenInstruction: web3.TransactionInstruction | null = null
 
-  const mintClient = getMintClient(anchorProvider, mintAddress)
-
   try {
-    await getTokenAccountInfo(mintClient, associatedTokenAccountAddress)
+    await getAccount(anchorProvider.connection, associatedTokenAccountAddress)
   } catch (err) {
-    if (!(err instanceof Error) || err.message !== 'Failed to find account') {
+    if (!(err instanceof TokenAccountNotFoundError)) {
       throw err
     }
 
-    createAssociateTokenInstruction = Token.createAssociatedTokenAccountInstruction(
-      ASSOCIATED_TOKEN_PROGRAM_ID,
-      TOKEN_PROGRAM_ID,
-      mintAddress,
+    createAssociateTokenInstruction = createAssociatedTokenAccountInstruction(
+      payerAddress ?? ownerAddress,
       associatedTokenAccountAddress,
       ownerAddress,
-      payerAddress ?? ownerAddress,
+      mintAddress,
+      TOKEN_PROGRAM_ID,
+      ASSOCIATED_TOKEN_PROGRAM_ID,
     )
   }
 
