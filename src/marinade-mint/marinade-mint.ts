44f72e7fcb17711f973c9f2167b20f97a22617f2
MjCage <lifepathapps@gmail.com>
2022-09-07 14:54:33 +0200

    chore: reverted spl-token bump

--- src/marinade-mint/marinade-mint.ts
+++ src/marinade-mint/marinade-mint.ts
@@ -1,5 +1,6 @@
-import { BN, Provider, web3 } from '@project-serum/anchor'
-import { getMint, Mint } from '@solana/spl-token'
+import { Provider, web3 } from '@project-serum/anchor'
+import { MintInfo, Token } from '@solana/spl-token'
+import { getMintClient } from '../util/anchor'
 import { tokenBalanceToNumber } from '../util/conversion'
 
 export class MarinadeMint {
@@ -12,20 +13,23 @@ export class MarinadeMint {
     return new MarinadeMint(anchorProvider, mintAddress)
   }
 
+  mintClient = (): Token => getMintClient(this.anchorProvider, this.address)
+  mintInfo = (): Promise<MintInfo> => this.mintClient().getMintInfo()
+
   /**
    * Returns Total supply as a number with decimals
    * @param mintInfoCached optional
    * @returns 
    */
-  async totalSupply(mintInfoCached?: Mint): Promise<number> {
-    const mintInfo = mintInfoCached ?? await getMint(this.anchorProvider.connection, this.address)
-    return tokenBalanceToNumber(new BN(mintInfo.supply.toString()), mintInfo.decimals)
+  async totalSupply(mintInfoCached?: MintInfo): Promise<number> {
+    const mintInfo = mintInfoCached ?? await this.mintInfo()
+    return tokenBalanceToNumber(mintInfo.supply, mintInfo.decimals)
   }
 
   /**
    * @deprecated use totalSupply() instead
    */
-  async tokenBalance(mintInfoCached?: Mint): Promise<number> {
+  async tokenBalance(mintInfoCached?: MintInfo): Promise<number> {
     return this.totalSupply(mintInfoCached)
   }
 
