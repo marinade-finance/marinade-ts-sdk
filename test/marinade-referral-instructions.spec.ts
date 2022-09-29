da7a073e4b6cd27ef0a1d9cf5e1e25e9ff04a5b5
MjCage <lifepathapps@gmail.com>
2022-09-29 15:06:15 +0200

    feat: added liquidate stake acc referral test

--- test/marinade-referral-instructions.spec.ts
+++ test/marinade-referral-instructions.spec.ts
@@ -1,3 +1,5 @@
+import { web3 } from '@project-serum/anchor'
+
 import { Marinade, MarinadeConfig, MarinadeUtils } from '../src'
 import * as TestWorld from './test-world'
 
@@ -35,6 +37,21 @@ describe('Marinade Referral', () => {
       const { transaction } = await marinade.liquidUnstake(MarinadeUtils.solToLamports(0.8))
       const transactionSignature = await TestWorld.PROVIDER.send(transaction)
       console.log('Liquid unstake tx:', transactionSignature)
+    })
+  })
+
+  describe.skip('liquidateStakeAccount', () => {
+    it('liquidates stake account to SOL', async() => {
+      const config = new MarinadeConfig({
+        connection: TestWorld.CONNECTION,
+        publicKey: TestWorld.SDK_USER.publicKey,
+        referralCode: TestWorld.REFERRAL_CODE,
+      })
+      const marinade = new Marinade(config)
+
+      const { transaction } = await marinade.liquidateStakeAccount(new web3.PublicKey("Cp4JSgUrPVaqMkr6m4KEu5yyu1khPTHQHrMNn5wfQabp"))
+      const transactionSignature = await TestWorld.PROVIDER.send(transaction)
+      console.log('Liquididate stake account tx:', transactionSignature)
     })
   })
 })
