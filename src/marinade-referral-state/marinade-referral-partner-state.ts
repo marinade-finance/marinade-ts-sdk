6ad8b611ae35b64da4c6948d57584bda4da70341
phola <info@phola.com>
2022-10-12 16:56:25 +0100

    feat: adds getReferralPartners and code arg

--- src/marinade-referral-state/marinade-referral-partner-state.ts
+++ src/marinade-referral-state/marinade-referral-partner-state.ts
@@ -1,22 +1,31 @@
-import { web3 } from '@project-serum/anchor'
-import { Marinade } from '../marinade'
-import { MarinadeReferralStateResponse } from './marinade-referral-state.types'
+import { web3 } from "@project-serum/anchor"
+import { Marinade } from "../marinade"
+import { MarinadeReferralStateResponse } from "./marinade-referral-state.types"
 
 export class MarinadeReferralPartnerState {
   private constructor(
     public readonly state: MarinadeReferralStateResponse.ReferralState,
     public readonly referralStateAddress: web3.PublicKey,
-    public readonly marinadeReferralProgramId: web3.PublicKey,
-  ) { }
+    public readonly marinadeReferralProgramId: web3.PublicKey
+  ) {}
 
-  static async fetch(marinade: Marinade) {
+  static async fetch(marinade: Marinade, referralCode?: web3.PublicKey) {
     const { marinadeReferralProgram, config } = marinade
-
-    if (!config.referralCode) {
-      throw new Error('The Referral Code must be provided in the MarinadeConfig!')
+    const code = referralCode ?? config.referralCode
+    if (!code) {
+      throw new Error(
+        "The Referral Code must be provided in the MarinadeConfig or supplied as an arg!"
+      )
     }
-    const state = await marinadeReferralProgram.program.account.referralState.fetch(config.referralCode) as MarinadeReferralStateResponse.ReferralState
+    const state =
+      (await marinadeReferralProgram.program.account.referralState.fetch(
+        code
+      )) as MarinadeReferralStateResponse.ReferralState
 
-    return new MarinadeReferralPartnerState(state, config.referralCode, config.marinadeReferralProgramId)
+    return new MarinadeReferralPartnerState(
+      state,
+      code,
+      config.marinadeReferralProgramId
+    )
   }
 }
