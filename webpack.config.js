70957b095e8a8e442dfc66acfaccd6203bc9b281
MjCage <lifepathapps@gmail.com>
2023-04-19 11:08:30 +0200

    fix: webpack error

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,19 @@ const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.ts',
   module: {
     rules: [
       {
         test: /\.[jt]sx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            allowTsInNodeModules: true,
+          },
+        },
+        exclude: /node_modules(?!\/@marinade\.finance\/directed-stake-sdk)/,
       },
     ],
   },
