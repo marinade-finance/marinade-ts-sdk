eaae135731a1b26327c5270236a8c56851183ad7
Ondra Chaloupka <chalda@marinade.finance>
2026-05-06 00:09:45 +0200

    [package.json] bump bn version and subsequent version bumps to fix tests

--- src/marinade-state/marinade-state.ts
+++ src/marinade-state/marinade-state.ts
@@ -235,8 +235,8 @@ export class MarinadeState {
     // The data's first 4 bytes are: u8 0x0 0x0 0x0 but borsh uses only the first byte to find the enum's value index.
     // The next 3 bytes are unused and we need to get rid of them (or somehow fix the BORSH schema?)
     const adjustedData = Buffer.concat([
-      data.subarray(0, 1), // the first byte indexing the enum
-      data.subarray(4, data.length), // the first byte indexing the enum
+      new Uint8Array(data.subarray(0, 1)), // the first byte indexing the enum
+      new Uint8Array(data.subarray(4, data.length)),
     ])
     return deserializeUnchecked(
       STAKE_STATE_BORSH_SCHEMA,
